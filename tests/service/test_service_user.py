fa7b28773f0713b50bebfbbfaaf8e5fa1601e4b5
Carlos Eduardo <carlos_eduardo_a.r@outlook.com>
2024-10-31 20:17:25 -0300

    feat: adicao de casos de teste para falhas

--- tests/service/test_service_user.py
+++ tests/service/test_service_user.py
@@ -2,6 +2,24 @@ from src.service.service_user import ServiceUser
 
 class TestServiceUser:
 
+
+###########  Usuario ja cadastrado  ###########
+
+    def test_add_user_already_registered(self):
+        name_add = 'Leonardo'
+        job_add = 'TechLead'
+        result_expect = 'Usuario ja cadastrado'
+        service = ServiceUser()
+
+        service.add_user(name=name_add, job=job_add)
+
+        result = service.add_user(name=name_add, job=job_add)
+
+        assert result == result_expect
+
+
+###########  Usuário adicionado com sucesso  ###########
+
     def test_add_user_success(self):
         name_add = 'Leonardo'
         job_add = 'TechLead'
@@ -12,6 +30,21 @@ class TestServiceUser:
 
         assert result_expect == result
 
+
+###########  Nome ou Profissão precisa ser um texto  ###########
+
+    def test_add_user_invalid_name_or_job(self):
+        result_expect = 'Nome ou Profissão precisa ser um texto'
+        service = ServiceUser()
+        
+        invalid_name = 12345
+        job = 'Analista de Sistemas'
+        result = service.add_user(name=invalid_name, job=job)
+        assert result == result_expect
+
+
+###########  Usuario nao adicionado  ###########
+
     def test_validate_null_job(self):
         name_null = 'Matheus'
         job_null = None
@@ -21,6 +54,9 @@ class TestServiceUser:
         result = service.add_user(name=name_null, job=job_null)
 
         assert result_expect == result
+
+
+###########  Profissão atualizada com sucesso  ###########
 
     def test_update_user_success(self):
         name_update = "Leonardo"
@@ -34,6 +70,9 @@ class TestServiceUser:
 
         assert result_expect == result
 
+
+###########  Selecionar User  ###########
+
     def test_search_user_success(self):
         name_to_search = "Leonardo"
         result_expect = "Profissão: " + "TechLead"
@@ -45,13 +84,28 @@ class TestServiceUser:
 
         assert result_expect == result
 
+
+###########  Deletar usuário  ###########
+
     def test_delete_user_success(self):
         name_to_delete = "Leonardo"
         result_expect = "Usuario removido"
         service = ServiceUser()
-        
+
         service.add_user(name=name_to_delete, job='TechLead')
 
         result = service.del_user(name=name_to_delete)
 
         assert result_expect == result
+
+
+###########  Usuário não encontrado  ###########
+
+    def test_user_not_found(self):
+        name_not_found = 'teste-teste'
+        result_expect = 'Usuario não encontrado'
+        service = ServiceUser()
+
+        result = service.del_user(name=name_not_found)
+
+        assert result == result_expect
